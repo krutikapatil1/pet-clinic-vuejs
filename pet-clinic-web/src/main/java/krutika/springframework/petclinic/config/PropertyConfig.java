1d90f07e5884bfe37be1a8d4b4094dfc0e945548
Krutika Patil <krutiks89@gmail.com>
2020-06-06 19:47:38 -0400

    Added PetTypes in with Bootstrap (startup). Closes #33.

--- pet-clinic-web/src/main/java/krutika/springframework/petclinic/config/PropertyConfig.java
+++ pet-clinic-web/src/main/java/krutika/springframework/petclinic/config/PropertyConfig.java
@@ -1,7 +1,6 @@
 package krutika.springframework.petclinic.config;
 
 import krutika.springframework.petclinic.beans.MessageHlpr;
-import org.hibernate.pretty.MessageHelper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
