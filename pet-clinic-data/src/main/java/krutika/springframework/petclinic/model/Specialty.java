e5813c8b150fdbcdfab875eebe57ff1035120148
Krutika Patil <krutiks89@gmail.com>
2020-06-08 12:41:09 -0400

    Added JPA mappings to Vet Entity. Closes #38.

--- pet-clinic-data/src/main/java/krutika/springframework/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/krutika/springframework/petclinic/model/Specialty.java
@@ -1,11 +1,18 @@
 package krutika.springframework.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author : Krutika Patil
  * @since : 6/6/2020, Sat
  **/
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
