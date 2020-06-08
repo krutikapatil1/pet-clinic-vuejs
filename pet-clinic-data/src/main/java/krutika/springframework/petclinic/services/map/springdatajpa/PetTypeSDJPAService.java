f33dd66caf17fcfb4d31b59ac6c67d7842822549
Krutika Patil <krutiks89@gmail.com>
2020-06-08 13:44:28 -0400

    Created Spring Data JPA Pet Type Service. Closes #43.

---
+++ pet-clinic-data/src/main/java/krutika/springframework/petclinic/services/map/springdatajpa/PetTypeSDJPAService.java
@@ -0,0 +1,52 @@
+package krutika.springframework.petclinic.services.map.springdatajpa;
+
+import krutika.springframework.petclinic.model.PetType;
+import krutika.springframework.petclinic.repositories.PetTypeRepository;
+import krutika.springframework.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author : Krutika Patil
+ * @since : 6/8/2020, Mon
+ **/
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJPAService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    public PetTypeSDJPAService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> types = new HashSet<>();
+        petTypeRepository.findAll().forEach(types::add);
+        return types;
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return petTypeRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return petTypeRepository.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        petTypeRepository.delete(object);
+    }
+
+    @Override
+    public void deletebyId(Long aLong) {
+        petTypeRepository.deleteById(aLong);
+    }
+}
