e39e826c7b546698fd0770d48efd47d50091c6aa
Krutika Patil <krutiks89@gmail.com>
2020-06-08 12:23:42 -0400

    Added JPA mappings to Owner Entity. Closes #37.

--- pet-clinic-data/src/main/java/krutika/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/krutika/springframework/petclinic/model/Pet.java
@@ -1,13 +1,30 @@
 package krutika.springframework.petclinic.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity{
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+
     @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
