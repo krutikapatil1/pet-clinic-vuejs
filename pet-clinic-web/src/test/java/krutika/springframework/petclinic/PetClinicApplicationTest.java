5285baad986129eba11efdccc7ae5a6c6f1092d3
Krutika Patil <krutiks89@gmail.com>
2020-06-10 23:15:44 -0400

    Upgraded to JUnit 5. Closes #4.

---
+++ pet-clinic-web/src/test/java/krutika/springframework/petclinic/PetClinicApplicationTest.java
@@ -0,0 +1,20 @@
+package krutika.springframework.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * @author : Krutika Patil
+ * @since : 6/10/2020, Wed
+ **/
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PetClinicApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
